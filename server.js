bc9bb13ff0aaf3567eeb297a04ade0fcd55c1663
datnt <mrdatngo@gmail.com>
2021-10-17 20:48:42 +0700

    update port

--- server.js
+++ server.js
@@ -64,6 +64,24 @@ server.use((req, res, next) => {
 
 server.use(router)
 
-server.listen(3001, () => {
-    console.log('JSON Server is running at port 3001')
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+var port = normalizePort(process.env.PORT || '3002');
+
+server.listen(port, () => {
+    console.log('JSON Server is running at port', port)
 })
